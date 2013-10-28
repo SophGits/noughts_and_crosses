6973940da4994eba5a2edfd158c78406d38c424b
SophGits <sophie.jiffy@gmail.com>
2013-10-28 16:37:17 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NoughtsAndCrosses::Application.config.secret_token = '631de23e59de073350670c4a0bdaffcfb89477bb7a85cfe0ec28abd2dd045bbd1276fa64391806d3bfacda019a72d9c9aac516c7b54e50e47d2e0f299f2af2b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NoughtsAndCrosses::Application.config.session_store :cookie_store, key: '_noughts_and_crosses_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NoughtsAndCrosses::Application.config.session_store :active_record_store
